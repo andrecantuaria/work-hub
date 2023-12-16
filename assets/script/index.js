4c32538a9150a867d98c211bbc7fde6aed0d47d5
Andre Cantuaria <andrecantuaria@gmail.com>
2023-12-16 13:04:29 -0600

    Create Profile page

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Dialog
 const btnJoinNow = document.querySelector('.btn-join-now');
 const dialog = document.querySelector('dialog');
 const closeModal = document.querySelector('span');
@@ -21,3 +22,43 @@ dialog.addEventListener('click', function(e) {
             dialog.close();
         } 
 });
+
+
+//  Get Ramdum User
+
+/*
+	Fetch API
+
+	fetch makes use of JavaScript Promises. The initial promise return a 
+	response object, which contains the full HTTP response, including the
+	the body, headers, status code, CORS type, and URL. With the response
+	returned, you can then use an additional parsing method to parse
+	the body of the response.
+*/
+
+
+// Random user generator API is a free, open-source API for generating random
+// user data. Like 'lorem ipsum', but for people.
+
+const URL = "https://randomuser.me/api/?nat=CA&results=10&seed=same";
+
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json; charset=utf-8",
+  },
+  mode: "cors",
+};
+
+
+// in this example I am using the json() method to parse the body as Json.
+//Note that despite the method being called json(), the result is not json but is
+//intead the result of taking JSON as input and parsing it to produce a
+//JavaScript object.
+
+
+fetch(URL, options)
+  .then((response) => response.json())
+  .then((json) => json.results)
+  .then(console.log)
+  .catch(console.error);
